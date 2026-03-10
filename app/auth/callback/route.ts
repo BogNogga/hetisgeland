bfa79e762d49cd946caa7a0ddf8f6b79eae109cf
BogNogga <77670819+BogNogga@users.noreply.github.com>
2026-03-10 21:44:54 +0100

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,4 +1,4 @@
-import { createServerClient } from '@supabase/ssr'
+import { createServerClient, type CookieMethodsServer } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
@@ -17,7 +17,7 @@ export async function GET(request: NextRequest) {
           getAll() {
             return cookieStore.getAll()
           },
-          setAll(cookiesToSet) {
+          setAll(cookiesToSet: Parameters<CookieMethodsServer['setAll']>[0]) {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
             )
